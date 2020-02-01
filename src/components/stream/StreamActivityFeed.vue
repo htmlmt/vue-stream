167f43f2c40d7da5512a1e0b2a64486277557a13
Michael Todd <mtodd@donordrive.com>
2020-01-31 18:38:48 -0600

    WIP: Adding Tailwind, started on activity feed

--- src/components/stream/StreamActivityFeed.vue
+++ src/components/stream/StreamActivityFeed.vue
@@ -1,34 +1,173 @@
 <template lang="html">
-    <aside class="streamer__activity-feed">
-        <div class="activity-feed__feed">
-            <ul class="feed__list" :style="{ width: ((items.length / 3) * 100) + '%' }">
-                <li :style="{ width: (100 / items.length) + '%' }" v-for="item in items" :key="item.createdDateUTC">
-                    <div class="item__badge">
-                        <img v-if="item.type === 'participantBadge'" :src="item.imageURL" :alt="item.title">
-                        <p v-if="item.type === 'donation'"><strong>{{ item.amount | formatMoney }}</strong></p>
+    <aside class="
+        flex
+        items-stretch
+        justify-between
+        pr-4
+        relative
+        z-0
+    ">
+        <div class="
+            activity-feed__feed
+
+            overflow-x-scroll
+            py-4
+        ">
+            <ul
+                class="
+                    feed__list
+
+                    flex
+                    items-stretch
+                    list-none
+                    relative
+                "
+                :style="
+                    {
+                        width: ((items.length / 3) * 100) + '%'
+                    }
+                "
+                v-cloak
+            >
+                <li
+                    class="
+                        pl-4
+                        relative
+                        w-1/3
+                        z-100
+
+                        lg:flex
+                        lg:items-stretch
+                        lg:static
+                    "
+                    :key="item.createdDateUTC"
+                    :style="
+                        {
+                            width: (100 / items.length) + '%'
+                        }
+                    "
+                    v-for="item in items"
+                >
+                    <div class="
+                        item__badge
+
+                        bg-white
+                        border
+                        border-b-0
+                        border-gray-400
+                        border-solid
+                        flex
+                        items-center
+                        justify-center
+                        rounded-t-sm
+
+                        lg:border-b
+                        lg:border-r-0
+                        lg:rounded-bl-sm
+                        lg:rounded-tr-none
+                    ">
+                        <img
+                            :alt="item.title"
+                            :src="item.imageURL"
+                            v-if="item.type === 'participantBadge'"
+                        />
+                        <p
+                            class="
+                                font-sans
+                                text-green-400
+                                m-0
+                            "
+                            v-if="item.type === 'donation'"
+                        >
+                            <strong>{{ item.amount | formatMoney }}</strong>
+                        </p>
                     </div>
-                    <div class="item__description">
-                        <p class="description__title">
+                    <div class="
+                        item__description
+
+                        bg-white
+                        border
+                        border-gray-400
+                        border-t-0
+                        flex
+                        items-start
+                        relative
+                        rounded-b-sm
+
+                        lg:border-t
+                        lg:flex-wrap
+                        lg:items-center
+                        lg:rounded-bl-none
+                        lg:rounded-r-sm
+                    ">
+                        <p class="
+                            description__title
+
+                            hidden
+
+                            lg:block
+                            lg:gray-700
+                            lg:m-0
+                            lg:overflow-hidden
+                            lg:whitespace-no-wrap
+                            lg:w-full
+                        ">
                             <strong>{{ item.title }}</strong>
                         </p>
-                        <p v-if="item.message" class="description__message">
+                        <p class="
+                            description__message
+
+                            hidden
+
+                            lg:block
+                            lg:text-gray-700
+                            lg:m-0
+                            lg:overflow-hidden
+                            lg:whitespace-no-wrap
+                            lg:w-full
+                        " v-if="item.message">
                             {{ item.message }}
                         </p>
-                        <p class="description__time">
+                        <p class="
+                            description__time
+
+                            m-0
+                            text-center
+                            text-gray-600
+                            w-full
+
+                            lg:absolute
+                            lg:text-right
+                            lg:w-auto
+                        ">
                             {{ item.createdDateUTC | formatDate }}
                         </p>
                     </div>
                 </li>
             </ul>
         </div>
-        <div class="activity-feed__button">
-            <Button>
+        <div class="
+            activity-feed__button
+
+            flex
+            items-center
+            py-4
+        ">
+            <!-- <Button class="
+                flex
+                items-center
+                px-4
+
+                border
+                border-gray-400
+                rounded-sm
+            ">
                 <Icon
                 icon-name="donate"
                 >
                     <g v-html="getIconByName('donate')"></g>
                 </Icon>
-            </Button>
+            </Button> -->
         </div>
     </aside>
 </template>
@@ -36,9 +175,9 @@
 <script>
 import FetchIcons from '@/data/fetch-icons';
 
-import Icon from '@/components/atoms/Icon.vue';
+// import Icon from '@/components/atoms/Icon.vue';
 
-import Button from '@/components/atoms/Button.vue';
+// import Button from '@/components/atoms/Button.vue';
 
 import moment from 'moment';
 import numeral from 'numeral';
@@ -87,8 +226,8 @@ export default {
         }
     },
     components: {
-        Button,
-        Icon,
+        // Button,
+        // Icon,
     },
 };
 </script>
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    theme: {
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '992px',
+            'xl': '1280px',
+        },
+        fontFamily: {
+            sans: ['Futura LT', 'sans-serif'],
+        },
+        colors: {
+            gray: {
+                '400': '#dddddd',
+                '600': '#666666',
+                '700': '#444444',
+            },
+            green: {
+                '400': '#468302',
+            },
+        },
+        borderRadius: {
+            'none': '0',
+            'sm': '5px',
+            default: '.25rem',
+            'lg': '.5rem',
+            'full': '9999px',
+        },
+    },
+    variants: {},
+    plugins: []
+}
